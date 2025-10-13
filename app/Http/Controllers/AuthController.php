86753f4a637a49e75df2c515adc7ee4d358a44c7
mahdirnma <rahnamamahdi78@gmail.com>
2025-10-13 20:19:35 +0330

    create admin dashboard and login panel with auth process and logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginUserRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    public function loginForm(){
+        return view('auth.login');
+    }
+    public function login(LoginUserRequest $request){{
+        $myData=$request->only('email','password');
+        if (!Auth::attempt($myData)) {
+            return redirect()->route('login.form');
+        }
+        return redirect()->route('home');
+    }}
+    public function logout(){
+        Auth::logout();
+        return redirect()->route('login.form');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [UserController::class, 'index'])->name('home');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'loginForm'])->name('login.form');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
 });
